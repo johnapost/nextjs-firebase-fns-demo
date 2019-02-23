8475a53d02fbe42c5e829544f369d4a578c18377
John Apostol <notimetobeidle@gmail.com>
2019-02-23 16:49:25 -0600

    Add app pages

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+export default () =>
+  <>
+    <p>This page is served by the index function</p>
+    <ul>
+      <li>
+        <Link href='/nextjs'>Why NextJS?</Link>
+      </li>
+      <li>
+        <Link href='/serverless'>Why serverless mode?</Link>
+      </li>
+      <li>
+        <Link href='/firebase'>Why Firebase Functions?</Link>
+      </li>
+    </ul>
+  </>
